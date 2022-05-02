442d3a8889570e7106c62a0bcf7b9a03f04b9e40
Emily Gagne <ceg9498@gmail.com>
2022-05-02 15:16:31 -0400

    Allow importing template files.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 
-import createPilingExample from './piling-example.js';
+import { create } from './piling-example.js';
 
 export default function App() {
+  const [state, setState] = useState({});
   const pilingInitHandler = useCallback(async (element) => {
     if (!element) return;
-    const piling = await createPilingExample(element);
+    const piling = create(element, state);
     console.info(`Piling.js v${piling.version}`);
     return () => piling.destroy();
-  }, []);
+  }, [state]);
+
+  const settingsInput = React.createRef();
 
   return (
     <div className="App">
@@ -26,6 +29,16 @@ export default function App() {
           </a>{' '}
           in a React app.
         </p>
+        <div className='uploaders'>
+          <label>
+            Upload Settings:
+            <input type="file" accept=".json" ref={settingsInput} />
+          </label>
+          <button onClick={(e)=>{
+            e.preventDefault();
+            readSettings(settingsInput.current.files[0], setState);
+          }}>Load</button>
+        </div>
       </header>
       <main
         className="App-piling-wrapper"
@@ -34,3 +47,19 @@ export default function App() {
     </div>
   );
 };
+
+function readSettings(file, update) {
+  if(!file || !file.type === "application/json") {
+    alert("Please select a JSON file to load.");
+  } else {
+    const reader = new FileReader();
+    reader.addEventListener('error', (e) => {
+      console.log("Error loading file:", e.target.result);
+    });
+    reader.addEventListener('load', (e) => {
+      const settings = JSON.parse(e.target.result);
+      update(settings);
+    });
+    reader.readAsText(file);
+  }
+}
--- src/piling-example.js
+++ src/piling-example.js
@@ -1,22 +1,39 @@
-import { createLibraryAsync, createImageRenderer } from 'piling.js';
+import {
+  //createLibrary,
+  //createLibraryFromState,
+  createImageRenderer,
+  createLibraryAsync
+} from 'piling.js';
 
-const create = (element) =>
-  createLibraryAsync(element, {
-    renderer: createImageRenderer(),
-    // prettier-ignore
-    items: [
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000253413.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000533739.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000314530.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000418512.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000454273.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000219654.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000558596.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000392493.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000115639.jpg' },
-      { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000228398.jpg' },
-    ],
-    darkMode: true,
-  });
+// prettier-ignore
+const items = [
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000253413.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000533739.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000314530.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000418512.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000454273.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000219654.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000558596.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000392493.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000115639.jpg' },
+  { src: 'https://storage.googleapis.com/pilingjs/coco-cars/000000228398.jpg' },
+];
 
-export default create;
+let piling;
+
+export const create = (el, state) => {
+  if(Object.keys(state).length !== 0) {
+    const piles = piling.get("piles");
+    const items = piling.get("items");
+    piling.importState({...state, piles, items, itemRenderer: createImageRenderer()});
+  } else {
+    piling = createLibraryAsync(el, {
+      renderer: createImageRenderer(),
+      items,
+      darkMode: false,
+    }).then(res => {
+      piling = res;
+    });
+  }
+  return piling;
+};
